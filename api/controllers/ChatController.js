925bf2a1656f350abf6e61858a8e19f90b973fe5
legiahoang <legiahoang@yahoo.com>
2017-06-23 10:35:59 +0700

    edit chat controller,chat model

--- api/controllers/ChatController.js
+++ api/controllers/ChatController.js
@@ -6,6 +6,35 @@
  */
 
 module.exports = {
-	
+    /*
+    i will convert query operation to waterline command
+    */
+    index: (req, res) => {
+        var data = req.params.all();
+        if (req.isSocket && req.method === 'POST') {
+            Chat.query('INSERT into "chat" ("user","message") VALUES("' + data.user + '","' + data.message + '")'
+                , (err, rows) => {
+                    if (err) {
+                        sails.log(err);
+                        sails.log("Error in db operation,if isSocket and POST method");
+                    } else {
+                        Chat.publishCreate({ id: rows.insertId, message: data.message, user: data.user });
+                    }
+                })
+        } else if (req.isSocket) {
+            Chat.watch(req.socket);
+            sails.log('User subcrible to ' + req.socket.id);
+        }
+        if (req.method === 'GET') {
+            Chat.query('SELECT * FROM "chat"', (err, rows) => {
+                if (err) {
+                    sails.log(err);
+                    sails.log("Error occurred in db operation,if method === GET!");
+                } else {
+                    res.send(rows);
+                }
+            });
+        }
+    }
 };
 
--- api/models/Chat.js
+++ api/models/Chat.js
@@ -8,7 +8,14 @@
 module.exports = {
 
   attributes: {
-
+    user: {
+      type: 'string',
+      required: true
+    },
+    message: {
+      type: 'string',
+      required: true
+    }
   }
 };
 
