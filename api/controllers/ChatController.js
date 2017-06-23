4ca89a9b9c110589bb84dc8cebc39fb230cc383e
legiahoang <legiahoang@yahoo.com>
2017-06-23 11:48:25 +0700

    add font end logic and fix backend query

--- api/controllers/ChatController.js
+++ api/controllers/ChatController.js
@@ -12,7 +12,7 @@ module.exports = {
     index: (req, res) => {
         var data = req.params.all();
         if (req.isSocket && req.method === 'POST') {
-            Chat.query('INSERT into "chat" ("user","message") VALUES("' + data.user + '","' + data.message + '")'
+            Chat.query('INSERT into `chat` (`user`,`message`) VALUES("' + data.user + '","' + data.message + '")'
                 , (err, rows) => {
                     if (err) {
                         sails.log(err);
@@ -26,7 +26,7 @@ module.exports = {
             sails.log('User subcrible to ' + req.socket.id);
         }
         if (req.method === 'GET') {
-            Chat.query('SELECT * FROM "chat"', (err, rows) => {
+            Chat.query('SELECT * FROM `chat`', (err, rows) => {
                 if (err) {
                     sails.log(err);
                     sails.log("Error occurred in db operation,if method === GET!");
